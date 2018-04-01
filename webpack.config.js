1a42b5cb1546910a1cc2b917670807a8662fb67c
kn1cht <ito.turukasiwa@gmail.com>
2018-04-01 22:43:15 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const globule = require('globule');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const targetTypes = { ejs : 'html', js : 'js' };
+
+const getEntriesList = (targetTypes) => {
+  const entriesList = {};
+  for(const [ srcType, targetType ] of Object.entries(targetTypes)) {
+    const filesMatched = globule.find([`**/*.${srcType}`, `!**/_*.${srcType}`], { cwd : `${__dirname}/src` });
+    
+    for(const srcName of filesMatched) {
+      const targetName = srcName.replace(new RegExp(`.${srcType}$`, 'i'), `.${targetType}`);
+      entriesList[targetName] = `${__dirname}/src/${srcName}`;
+    }
+  }
+  return entriesList;
+}
+
+const app = {
+  entry  : getEntriesList(targetTypes),
+  output : {
+    filename : '[name]',
+    path     : `${__dirname}/public`
+  },
+  module : {
+    rules : [
+      {
+        test : /\.ejs$/,
+        use  : [
+          {
+            loader  : 'html-loader',
+            options : { attrs : false }
+          },
+          'ejs-html-loader'
+        ]
+      }
+    ]
+  },
+  plugins : [
+    new CopyWebpackPlugin(
+      [{ from : `${__dirname}/src` }],
+      { ignore : Object.keys(targetTypes).map((ext) => `*.${ext}`) }
+    ),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+  ]
+};
+
+for(const [ targetName, srcName ] of Object.entries(getEntriesList({ ejs : 'html' }))) {
+  app.plugins.push(new HtmlWebpackPlugin({
+    filename : targetName,
+    template : srcName
+  }));
+}
+
+module.exports = app;
